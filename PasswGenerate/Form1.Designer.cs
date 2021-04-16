c6f0d42ba5114012bc850b0c13c88fb5678be64c
AlexJones16 <82084806+AlexJones16@users.noreply.github.com>
2021-04-16 15:03:57 +0300

    Добавьте файлы проекта.

---
+++ PasswGenerate/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace PasswGenerate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(217, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(246, 137);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(217, 155);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 46);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Генерировать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(360, 155);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 46);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Очистить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 22);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(169, 17);
+            this.radioButton1.TabIndex = 4;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Добавить Заглавные Буквы";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(12, 46);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(153, 17);
+            this.radioButton2.TabIndex = 5;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Добавить Спец Символы";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(12, 70);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(113, 17);
+            this.radioButton3.TabIndex = 6;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Сложный Пароль";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(17, 111);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(164, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Сбросить настройки";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 213);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Генератор паролей";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ PasswGenerate/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PasswGenerate
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();         
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string passw = "1234567890abcdefghijkmnopqrstuwxyz";
+            string passWUpper = "1234567890abcdefghijkmnopqrstuwxyzABCDEFGHIJKMNOPQRSTUWXYZ";
+            string passWSpecSymbols = "1234567890abcdefghijkmnopqrstuwxyz!@#$%^&*()_+|[]{}?><";
+            string passStrongest = "1234567890abcdefghijkmnopqrstuwxyzABCDEFGHIJKMNOPQRSTUWXYZ!@#$%^&*()_+|[]{}?><";
+            char[] password = passw.ToCharArray();
+            char[] passwordWUpper = passWUpper.ToCharArray();
+            Random rn = new Random();
+            try
+            {
+                if (radioButton1.Checked == true)
+                {
+                    for (int i = 0; i < 11; i++)
+                    {
+                        richTextBox1.Text += passwordWUpper[rn.Next(passwordWUpper.Length)].ToString();              
+                    }
+                    richTextBox1.AppendText(Environment.NewLine);
+                }
+                else if (radioButton2.Checked == true)
+                {
+                    for (int i = 0; i < 11; i++)
+                    {
+                        richTextBox1.Text += passWSpecSymbols[rn.Next(passWSpecSymbols.Length)].ToString();
+                    }
+                    richTextBox1.AppendText(Environment.NewLine);
+                }
+                else if (radioButton3.Checked == true)
+                {
+                    for (int i = 0; i < 11; i++)
+                    {
+                        richTextBox1.Text += passStrongest[rn.Next(passStrongest.Length)].ToString();
+                    }
+                    richTextBox1.AppendText(Environment.NewLine);
+                }
+                else
+                {
+                    for (int i = 0; i < 11; i++)
+                    {
+                        richTextBox1.Text += password[rn.Next(password.Length)].ToString();
+                    }
+                    richTextBox1.AppendText(Environment.NewLine);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка!");
+            }
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+            radioButton3.Checked = false;
+        }
+    }
+}
